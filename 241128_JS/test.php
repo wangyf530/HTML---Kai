003ddd7ebd61eca0e29eb9ccb1486e6f445890e1
yf wang <wangyf530@gmail.com>
2024-11-28 11:34:16 +0800

    java

---
+++ 241128_JS/test.php
@@ -0,0 +1,14 @@
+<?php
+$name = 'a-kai';
+echo "Hi $name";
+
+// index array
+$index = [1,2,3];
+
+// associated array
+$assoc = [
+    's1' => 'Amy',
+    's2' => 'Bob',
+    's3' => 'Cole'
+];
+?>
---
+++ 老師存檔/s20241128/test.php
@@ -0,0 +1,18 @@
+<?php 
+    $name = 'a-kai';
+    echo("Hi $name");
+
+    // array
+
+    // index array
+    $indexArr = [1,2,3];
+    
+    // Associative array
+    $assocArr = [
+        's1' => 'amy',
+        's2' => 'bob',
+        's3' => 'cat'
+    ];
+
+
+?>
